9839df3779b1be221b352d0fa45b96d215278e56
ArziTech <wawcam123@gmail.com>
2025-03-23 10:01:05 +0700

    adding files

---
+++ script.js
@@ -0,0 +1,7 @@
+// Simple JavaScript for interactivity
+document.addEventListener('DOMContentLoaded', () => {
+    const header = document.querySelector('header');
+    header.addEventListener('click', () => {
+      alert('You clicked the header!');
+    });
+  });
